cbe1566493bb42f18ba895fd6caf4a40f0b81b10
VietDuong <duongquocviet@gmail.com>
2024-06-06 09:00:04 +0700

    Implement components icon add, edit and taskcounter

--- practice-one/src/components/TaskItem/TaskItem.js
+++ practice-one/src/components/TaskItem/TaskItem.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './taskItem.css';
 import Button from '../common/Button';
 import Input from '../common/Input';
+import EditIcon from '../common/Icon/EditIcon';
 
 const TaskItem = ({ task, deleteTask, editTask }) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -71,12 +72,7 @@ const TaskItem = ({ task, deleteTask, editTask }) => {
       </div>
       {!isEditing && (
         <Button className='btn-edit-task' onClick={handleEditClick}>
-          <svg width="24" height="24">
-            <g fill="none" fillRule="evenodd">
-              <path fill="currentColor" d="M9.5 19h10a.5.5 0 1 1 0 1h-10a.5.5 0 1 1 0-1z"></path>
-              <path stroke="currentColor" d="M4.42 16.03a1.5 1.5 0 0 0-.43.9l-.22 2.02a.5.5 0 0 0 .55.55l2.02-.21a1.5 1.5 0 0 0 .9-.44L18.7 7.4a1.5 1.5 0 0 0 0-2.12l-.7-.7a1.5 1.5 0 0 0-2.13 0L4.42 16.02z"></path>
-            </g>
-          </svg>
+          <EditIcon />
         </Button>
       )}
     </div>
---
+++ practice-one/src/components/common/Icon/AddIcom.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddIcon = () => (
+  <span className="icon-add" aria-hidden="true">
+    <svg width="13" height="13">
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z"
+      />
+    </svg>
+  </span>
+);
+
+export default AddIcon;
