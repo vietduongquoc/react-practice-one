81e64e5a3eb2cca66f68f3f8e65b1464bd1dfb74
VietDuong <duongquocviet@gmail.com>
2024-06-06 16:00:52 +0700

    Update code in error component

--- practice-one/src/components/Error/ErrorComponent.js
+++ practice-one/src/components/Error/ErrorComponent.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ErrorComponent = () => {
     return (
-        <div className={"className"}>
-            <h2 className={"title-erorr"}>Oops!</h2>
-            <p className={"description-error"}>Something went wrong...</p>
+        <div className="error-container">
+            <h2 className="title-error">Oops!</h2>
+            <p className="description-error">Something went wrong...</p>
         </div>
     );
 }
